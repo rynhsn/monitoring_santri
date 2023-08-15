0ee4c88294d48f5c2ee0f61b2fefa00d2f31f9c1
Riyan Husen <riyan.husen>
2023-08-15 17:34:06 +0700

    ok

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -46,14 +46,15 @@ class AuthController extends Controller
         // No need to show a login form if the user
         // is already logged in.
         if ($this->auth->check()) {
-            $redirectURL = session('redirect_url') ?? site_url('/');
+            $redirectURL = session('redirect_url') ?? site_url('/dashboard');
             unset($_SESSION['redirect_url']);
 
             return redirect()->to($redirectURL);
         }
 
         // Set a return URL if none is specified
-        $_SESSION['redirect_url'] = session('redirect_url') ?? previous_url() ?? site_url('/');
+        $_SESSION['redirect_url'] = session('redirect_url') ?? site_url('/dashboard');
+//        $_SESSION['redirect_url'] = session('redirect_url') ?? previous_url() ?? site_url('/dashboard');
 
         return $this->_render($this->config->views['login'], ['config' => $this->config]);
     }
@@ -93,7 +94,7 @@ class AuthController extends Controller
             return redirect()->to(route_to('reset-password') . '?token=' . $this->auth->user()->reset_hash)->withCookies();
         }
 
-        $redirectURL = session('redirect_url') ?? site_url('/');
+        $redirectURL = session('redirect_url') ?? site_url('/dashboard');
         unset($_SESSION['redirect_url']);
 
         return redirect()->to($redirectURL)->withCookies()->with('message', lang('Auth.loginSuccess'));
